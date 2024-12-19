95108de88e29e541e74f0698017bb9334f0ebd6b
TRIAL-G <jerotichsharon30@gmail.com>
2024-12-19 09:47:51 +0300

    sort in ascending order

---
+++ sort.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+import "sort"
+func main(){
+ a:= []int{5, 3, 4, 7, 8, 9}
+sort.Slice(a, func(i, j int) bool {
+    return a[i] < a[j]
+})
+for _, v := range a {
+    fmt.Println(v)
+}
+}
